c76ef8d776a5d042f82ff94ee7e4d57f01a4a001
Ame0thyst <kokoo15011209@gmail.com>
2024-12-22 00:23:44 +0700

    💄style : tipis tipis  �

--- apriori.php
+++ apriori.php
@@ -174,6 +174,8 @@ if (!isset($_GET['tanggal_akhir'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Analisis Apriori - Toko Harian</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+
 </head>
 
 <body>
